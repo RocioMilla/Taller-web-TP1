9896bbc1eba7df6132e695337ae5c7f498d6a2ad
RocioMilla <rocioaylenamilla@gmail.com>
2018-05-02 23:23:50 -0300

    Se agregaron las clases faltantes

--- src/main/java/ar/edu/unlam/tallerweb1/modelo/Barrio.java
+++ src/main/java/ar/edu/unlam/tallerweb1/modelo/Barrio.java
@@ -1,24 +1,30 @@
 
 package ar.edu.unlam.tallerweb1.modelo;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import javax.persistence.CascadeType;
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.OneToMany;
+import javax.persistence.ManyToOne;
+
+@Entity
 
 public class Barrio {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String nombre;
+	@ManyToOne
+	private Comuna Comuna;
 	
-	@OneToMany(mappedBy = "barrio", cascade=CascadeType.ALL)
-	private List<Direccion> Direccion = new ArrayList<>();
-	
+	public Barrio() {
+
+	}
+
+	public Barrio(String nombre) {
+		this.nombre = nombre;
+	}
+
 	public Long getId() {
 		return id;
 	}
@@ -31,6 +37,36 @@ public class Barrio {
 	public void setNombre(String nombre) {
 		this.nombre = nombre;
 	}
-	
-	
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((nombre == null) ? 0 : nombre.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Barrio other = (Barrio) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		if (nombre == null) {
+			if (other.nombre != null)
+				return false;
+		} else if (!nombre.equals(other.nombre))
+			return false;
+		return true;
+	}
+
 }
--- src/main/java/ar/edu/unlam/tallerweb1/modelo/Direccion.java
+++ src/main/java/ar/edu/unlam/tallerweb1/modelo/Direccion.java
@@ -1,18 +1,31 @@
 package ar.edu.unlam.tallerweb1.modelo;
 
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
 
+@Entity
 public class Direccion {
 		@Id
 		@GeneratedValue(strategy = GenerationType.IDENTITY)
 		private Long id;
 		private String calle;
 		private String numero;
+		
 		@ManyToOne
 		private Barrio barrio;
+		
+		public Direccion() {
+
+		}
+
+		public Direccion(String calle, String numero, Barrio barrio) {
+			this.calle = calle;
+			this.numero = numero;
+			this.barrio = barrio;
+		}		
 		
 		public Long getId() {
 			return id;
@@ -32,6 +45,5 @@ public class Direccion {
 		public void setNumero(String numero) {
 			this.numero = numero;
 		}
-		
 		
 }
