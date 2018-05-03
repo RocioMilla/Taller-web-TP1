6d5fe36b3999ae718c3b51c99476c77878139ad1
RocioMilla <rocioaylenamilla@gmail.com>
2018-05-03 00:18:39 -0300

    Se agrego test buscarFarmaciasPorCalle

--- src/test/java/ar/edu/unlam/tallerweb1/persistencia/TestFarmacia.java
+++ src/test/java/ar/edu/unlam/tallerweb1/persistencia/TestFarmacia.java
@@ -1,4 +1,6 @@
 package ar.edu.unlam.tallerweb1.persistencia;
+import static org.assertj.core.api.Assertions.assertThat;
+
 import java.util.List;
 
 import org.hibernate.criterion.Restrictions;
@@ -28,5 +30,28 @@ public class TestFarmacia extends SpringTest {
 		
 		List<Farmacia> Resultado = getSession().createCriteria(Farmacia.class)
 				.add(Restrictions.eq("diaDeTurno", "Martes")).list();
-	}
+		assertThat(Resultado).hasSize(1);
+
+		}
+	
+	@Test
+	@Transactional @Rollback(true)
+	public void buscarFarmaciasPorCalle() {
+		Punto geoLocalizacion1 = new Punto("1","1");
+		getSession().save(geoLocalizacion1);
+		Barrio barrio1 = new Barrio("Moron");
+		getSession().save(barrio1);
+		Direccion direccion1 = new Direccion("Calle falsa","123",barrio1);
+		getSession().save(direccion1);
+		Farmacia farmacia1 = new Farmacia("Farmacia1","11111","Martes",direccion1,geoLocalizacion1);
+		getSession().save(farmacia1);
+		
+		List<Farmacia> Resultado = getSession().createCriteria(Farmacia.class)
+			.createAlias("direccion", "direccionBuscada")
+			.add(Restrictions.eq("direccionBuscada.calle", "Calle falsa")).list();
+			assertThat(Resultado).hasSize(1);
+
+		}
+	
+	
 }
